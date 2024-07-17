53ee6a5135e8e6badaa57c346e698ca228dd7986
V1niciusDG <vinicius.dalanora@waterservicestech.com>
2024-07-17 17:19:54 -0300

    feat: create schema and initial config db

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "github.com/V1niciusDG/gopportunities/router"
+import (
+	"fmt"
+
+	"github.com/V1niciusDG/gopportunities/config"
+	"github.com/V1niciusDG/gopportunities/router"
+)
 
 func main() {
+
+	// initialize configs
+	err := config.Init()
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
+
 	//chama router
 	router.Initialize()
 }
