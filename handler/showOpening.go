7e6753a975677dc31310f8708bc50c177424293f
Vinicius <vdalanora19@gmail.com>
2024-08-12 11:51:34 -0300

    feat: add findByIdOpening handler logic

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -1,13 +1,23 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/V1niciusDG/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET Opening",
-	})
+	id := ctx.Param("id")
+	if id== "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "urlParameter").Error())
+		return
+	}
+	opening := schemas.Opening{}
+	if err := db.First(&opening, id).Error; err!=nil{
+		sendError(ctx, http.StatusNotFound,fmt.Sprintf("opening with id: %s not found", id))
+		return
+	} 
+	sendSucess(ctx, "show-openings", opening)
 }
